f5831f2961750d602efda88f8a189624d4d79936
tirugit-2001 <tirumaleshaeyadav@gmail.com>
2025-11-11 21:49:42 +0530

    added cors

--- src/app.ts
+++ src/app.ts
@@ -7,13 +7,27 @@ import { specs, swaggerUi } from "./config/swaggerConfig";
 import errorHandler from "./middlewares/errormiddleware";
 import cookieParser from "cookie-parser";
 const app = express();
+
+const allowedOrigins = [
+  "https://store.novastylesinterior.com",
+  "https://admin.novastylesinterior.com",
+  "https://novastylesinterior.com",
+];
+
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS blocked"));
+      }
+    },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
