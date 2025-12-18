7420907a3e74390a3bc72481e77cae766eb8e442
tirugit-2001 <tirumaleshaeyadav@gmail.com>
2025-12-18 21:05:27 +0530

    solved email service

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import config from "./config/config";
 import connectionToDb from "./config/db.config";
-
+import "./worker/email.worker";
 connectionToDb();
 app.listen(config.port, () => {
   console.log("Server is running on port", config.port);
